4894003af97fde35d31d6db588a75ab4b9f17e8d
Alexander <Philalex196401@gmail.com>
2019-04-03 00:24:36 +0300

    Created UIView Class

---
+++ testXib/testXib/TestXibView.swift
@@ -0,0 +1,21 @@
+//
+//  TestXibView.swift
+//  testXib
+//
+//  Created by Александр Филиппов on 03.04.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class TestXibView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
