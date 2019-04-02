ed1c3f6cdd9475bcee64ec26b0adc75fba3abc89
Alexander <Philalex196401@gmail.com>
2019-04-02 16:54:19 +0300

    Created  Xib file

---
+++ TestP/TestP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestP
+//
+//  Created by Александр Филиппов on 02.04.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
