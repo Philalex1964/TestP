9bff2fa8b6f96c5339bf3587f533b7ffb759a374
Alexander <Philalex196401@gmail.com>
2019-04-13 01:01:48 +0300

    Added outlet. Error
    deleted unused versions

--- testXib/testXib/TestXibView.swift
+++ testXib/testXib/TestXibView.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class TestXibView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var deliveryLabel: UILabel!
+    @IBOutlet weak var transportationButton: UIButton!
+    @IBOutlet weak var ownershipLabel: UILabel!
+    @IBOutlet weak var rentButton: UIButton!
+    
+    @IBOutlet weak var view: UIView!
+    
+    
+    
 }
